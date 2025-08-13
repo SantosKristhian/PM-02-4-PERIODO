14943e4a04423f8d259da28027cd41685ebacae3
SantosKristhian <gomess2012@live.com>
2025-08-13 19:29:31 -0300

    Spring Initialize

---
+++ src/main/java/com/example/EstoqueManager/EstoqueManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.EstoqueManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EstoqueManager/EstoqueManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EstoqueManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
