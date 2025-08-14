47e2e2056c3d815806265a61e1fff14e4da1ab89
SantosKristhian <gomess2012@live.com>
2025-08-14 00:56:52 -0300

    criação do mvc e endpoints do usuario

---
+++ src/main/java/com/example/EstoqueManager/model/UsuarioModel.java
@@ -0,0 +1,34 @@
+package com.example.EstoqueManager.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table (name = "usuario_table")
+@Getter
+@Setter
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private Integer idade;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Cargo cargo;
+}
