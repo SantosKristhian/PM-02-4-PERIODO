e30a7027dddc72f76459b54724a7d4602d969a87
Leozin <73923159+Thunderblack2829@users.noreply.github.com>
2025-08-16 05:14:40 -0300

    Relação vendas comprador

--- src/main/java/com/example/EstoqueManager/model/VendaModel.java
+++ src/main/java/com/example/EstoqueManager/model/VendaModel.java
@@ -1,7 +1,7 @@
+// VendaModel.java
 package com.example.EstoqueManager.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,7 +13,7 @@ import java.util.List;
 @Setter
 @Table(name="venda_table")
 public class VendaModel {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,8 +28,11 @@ public class VendaModel {
     @JoinColumn(name = "usuario_ID", nullable = false)
     private UsuarioModel usuario;
 
+    // 🔽 ADICIONE AQUI 👇
+    @ManyToOne
+    @JoinColumn(name = "comprador_id", nullable = false)
+    private CompradorModel comprador;
+
     @OneToMany(mappedBy = "venda", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ItemVendaModel> itens;
-
-
 }
