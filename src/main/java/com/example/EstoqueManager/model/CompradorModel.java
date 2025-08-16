7d658ee7ef8ff6ee154c06020eb03af331510229
Leozin <73923159+Thunderblack2829@users.noreply.github.com>
2025-08-16 04:46:05 -0300

    Comprador model

---
+++ src/main/java/com/example/EstoqueManager/model/CompradorModel.java
@@ -0,0 +1,30 @@
+package com.example.EstoqueManager.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name = "comprador_table")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompradorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "comprador", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<VendaModel> vendas;
+}
