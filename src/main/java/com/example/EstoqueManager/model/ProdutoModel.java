2315df0c3f84860456191260eb7947d531ffe10f
SantosKristhian <gomess2012@live.com>
2025-09-20 16:37:39 -0300

    produto model

--- src/main/java/com/example/EstoqueManager/model/ProdutoModel.java
+++ src/main/java/com/example/EstoqueManager/model/ProdutoModel.java
@@ -1,43 +1,43 @@
-package com.example.EstoqueManager.model;
+    package com.example.EstoqueManager.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
-import lombok.*;
+    import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+    import jakarta.persistence.*;
+    import lombok.*;
 
-import java.time.LocalDateTime;
+    import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "produto_table")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ProdutoModel {
+    @Entity
+    @Table(name = "produto_table")
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class ProdutoModel {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    @Column(nullable = false)
-    private String nome;
+        @Column(nullable = false)
+        private String nome;
 
-    @Column(nullable = false)
-    private Integer quantidade;
+        @Column(nullable = false)
+        private Integer quantidade;
 
-    @Column(nullable = false)
-    private Double preco;
+        @Column(nullable = false)
+        private Double preco;
 
-    @ManyToOne
-    @JsonIgnoreProperties("produtos")
-    @JoinColumn(name = "pcategoriaId", nullable = false)
-    private CategoriaModel categoria;
-
-
-    @ManyToOne
-    @JsonIgnoreProperties({"cpf", "idade", "login", "senha", "cargo", "vendas"})
-    @JoinColumn(name = "usuarioUltimaAlteracaoId")
-    private UsuarioModel usuarioUltimaAlteracao;
+        @ManyToOne
+        @JsonIgnoreProperties("produtos")
+        @JoinColumn(name = "pcategoriaId", nullable = false)
+        private CategoriaModel categoria;
 
 
-    private LocalDateTime dataUltimaAlteracao;
-}
+        @ManyToOne
+        @JsonIgnoreProperties({"cpf", "idade", "login", "senha", "cargo", "vendas"})
+        @JoinColumn(name = "usuarioUltimaAlteracaoId")
+        private UsuarioModel usuarioUltimaAlteracao;
+
+
+        private LocalDateTime dataUltimaAlteracao;
+    }
