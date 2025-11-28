d2880ec0f101e8234f9e93b2240ee9a5418b1faa
Leozin <73923159+Thunderblack2829@users.noreply.github.com>
2025-11-28 04:20:59 -0300

    Update CategoriaRepository.java

--- src/main/java/com/example/EstoqueManager/repository/CategoriaRepository.java
+++ src/main/java/com/example/EstoqueManager/repository/CategoriaRepository.java
@@ -4,6 +4,14 @@ import com.example.EstoqueManager.model.CategoriaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional; // <--- NOVA IMPORTAÇÃO
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
+
+    // NOVO: Método para checar duplicidade no SAVE
+    Optional<CategoriaModel> findByNome(String nome);
+
+    // NOVO: Método para checar duplicidade no UPDATE
+    Optional<CategoriaModel> findByNomeAndIdNot(String nome, Long id);
 }
